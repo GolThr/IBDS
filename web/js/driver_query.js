2d8a42fb8ebd471b91de9f1141be7eb58de0cefe
GolThr <1344006993@qq.com>
2019-08-05 23:47:54 +0800

    GolThr(8005)

---
+++ web/js/driver_query.js
@@ -0,0 +1,33 @@
+function init() {
+    $("#driver_query").css({'border-bottom':'rgba(255, 255, 255, 1) solid 5px'});
+}
+
+function onFuncPhone() {
+    $(".driver_func_phone_content").stop();
+    $(".driver_func_phone_content").fadeIn(500);
+}
+
+function outFuncPhone() {
+    $(".driver_func_phone_content").stop();
+    $(".driver_func_phone_content").fadeOut(250);
+}
+
+function onFuncSignIn() {
+    $(".driver_func_signin_content").stop();
+    $(".driver_func_signin_content").fadeIn(500);
+}
+
+function outFuncsignIn() {
+    $(".driver_func_signin_content").stop();
+    $(".driver_func_signin_content").fadeOut(250);
+}
+
+function onFuncNote() {
+    $(".driver_func_note_content").stop();
+    $(".driver_func_note_content").fadeIn(500);
+}
+
+function outFuncNote() {
+    $(".driver_func_note_content").stop();
+    $(".driver_func_note_content").fadeOut(250);
+}
---
+++ web/js/header_driver.js
@@ -0,0 +1,18 @@
+document.writeln("<div class=\"default_header\">\n" +
+    "        <div class=\"default_header_body\">\n" +
+    "            <div class=\"default_header_left\">\n" +
+    "                <img class=\"logo\" src=\"images/logo_line.png \" onclick=\"\"/>\n" +
+    "                <span class=\"default_title\">智能公交调度系统</span>\n" +
+    "            </div>\n" +
+    "            <div class=\"default_func_line\">\n" +
+    "                <ul class=\"default_func\">\n" +
+    "                    <li id=\"driver_query\" onclick=\"location.href='/IBDS/manager_upload.html'\">发车查询</li>\n" +
+    "                    <li id=\"driver_mine\" onclick=\"location.href='/IBDS/manager_mine.html'\">个人中心</li>\n" +
+    "                </ul>\n" +
+    "            </div>\n" +
+    "            <div class=\"default_user_body\">\n" +
+    "                <img class=\"manager_head\" src=\"images/head_default_manager.png \" onclick=\"\"/>\n" +
+    "                <span class=\"manager_name\">Driver</span>\n" +
+    "            </div>\n" +
+    "        </div>\n" +
+    "    </div>");
