f20faca1c453c2e9677176e90a60a94689e4d5de
GolThr <1344006993@qq.com>
2019-08-11 20:20:37 +0800

    GolThr(8011)

--- web/js/driver_mine.js
+++ web/js/driver_mine.js
@@ -180,17 +180,8 @@ function updateSession(data) {
     user_info.company = data.company;
     user_info.address = data.address;
     sessionStorage.setItem("user_info", JSON.stringify(user_info));
-    location.reload();
+    initManagerInfo();
     showTip("信息修改成功！");
-    $('.calendar').hide();
-    $("#driver_menu_personal").css({'background':'rgba(125, 197, 193, 0.1)'});
-    $("#driver_menu_changepwd").css({'background':'rgba(125, 197, 193, 0)'});
-    $("#driver_menu_logs").css({'background':'rgba(125, 197, 193, 0)'});
-    $("#driver_menu_notes").css({'background':'rgba(125, 197, 193, 0)'});
-    $(".driver_mine_personal_page").fadeIn(100);
-    $(".driver_mine_changepwd_page").hide();
-    $(".driver_mine_logs_page").hide();
-    $(".driver_mine_notes_page").hide();
 }
 
 function onShowTodayLogs(date) {
